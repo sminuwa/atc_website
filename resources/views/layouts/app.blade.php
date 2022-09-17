fa40e68209dfb26abc8266b068e78d362cb8a9d4
Sunusi M. Inuwa <sminuwa@yahoo.com>
2022-09-17 20:45:44 +0100

    asdfasf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,9 +100,9 @@
                          </div>--}}
                         <div class="main-header__top-social-box">
                             <div class="main-header__top-social">
-                                <a href="http://app.albabello.com">Memo</a>
-                                <a href="http://sage.albabello.com">Sage</a>
-                                <a href="mail.albabellogroup.com">Zimbra</a>
+                                <a target="_blank" href="http://app.albabello.com">Memo</a>
+                                <a target="_blank"  href="http://sage.albabello.com">Sage</a>
+                                <a target="_blank"  href="mail.albabellogroup.com">Zimbra</a>
                             </div>
                         </div>
                     </div>
