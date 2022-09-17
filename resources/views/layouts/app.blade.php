4a487de7471358ed82ae5ee24dab2abab888ee78
Sunusi M. Inuwa <sminuwa@yahoo.com>
2022-09-17 20:55:20 +0100

    asdfa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1392,8 +1392,9 @@
         <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
 
         <div class="logo-box">
-            <a href="index.html" aria-label="logo image"><img src="assets/images/resources/logo-2.png" width="143"
-                                                              alt="" /></a>
+            <a href="index.html" aria-label="logo image">
+                <img src="{{ asset('basic/logo.png') }}" width="150"  alt="" />
+            </a>
         </div>
         <!-- /.logo-box -->
         <div class="mobile-nav__container"></div>
@@ -1411,10 +1412,9 @@
         </ul><!-- /.mobile-nav__contact -->
         <div class="mobile-nav__top">
             <div class="mobile-nav__social">
-                <a href="#" class="fab fa-twitter"></a>
-                <a href="#" class="fab fa-facebook-square"></a>
-                <a href="#" class="fab fa-pinterest-p"></a>
-                <a href="#" class="fab fa-instagram"></a>
+                <a target="_blank" href="http://app.albabello.com">Memo</a>
+                <a target="_blank"  href="http://sage.albabello.com">Sage</a>
+                <a target="_blank"  href="http://mail.albabellogroup.com">Zimbra</a>
             </div><!-- /.mobile-nav__social -->
         </div><!-- /.mobile-nav__top -->
 
